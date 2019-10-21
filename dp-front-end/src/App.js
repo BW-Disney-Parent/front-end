71d831bf7696d27f5f53cb5654d97bdd775b6a04
Eric Richards <esholundr@yahoo.com>
2019-10-21 16:30:20 -0600

    Added Register component connected to Login page with route for it in App

--- dp-front-end/src/App.js
+++ dp-front-end/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Login from './components/Login';
+import Register from './components/Register';
 import './App.css';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <Router>
       <div className="App">
         <Route path="/login" component={Login} />
+        <Route path="/register-form" component={Register} />
       </div>
     </Router>
   );
