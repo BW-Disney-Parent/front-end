324839d15abe81a2f9586b7e5d5e2d7df0b28f5e
Amber Sorensen <ambersorensen@ambers-mbp.lan>
2019-10-22 15:29:09 -0600

    no more bugs!

--- dp-front-end/src/index.js
+++ dp-front-end/src/index.js
@@ -4,7 +4,7 @@ import {BrowserRouter as Router} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
-import {rootReducer} from './components/store/reducers';
+import rootReducer from './components/store/reducers';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
