d5a25aa7656d9ed19d990447b938ca916fff61e2
Amber Sorensen <ambersorensen@ambers-mbp.lan>
2019-10-22 14:10:27 -0600

    pushing commit to pull from master

--- dp-front-end/src/components/store/actions/index.js
+++ dp-front-end/src/components/store/actions/index.js
@@ -1,33 +1,34 @@
-// import axiosWithAuth from '.../utils/axiosWithAuth';
+import axiosWithAuth from '../utils/axiosWithAuth.js';
 
 
 
 
-// export const ADD_PARENT = 'ADD_PARENT';
-// export const FETCHING_MESSAGE_START = 'FETCHING_MESSAGE_START';
-// export const FETCHING_MESSAGE_SUCCESS = 'FETCHING_MESSAGE_SUCESS';
-// export const FETCHING_MESSAGE_FAILURE = 'FETCHING_MESSAGE_FAILURE';
 
-// export const getParent = () => dispatch => {
-//     dispatch({ type: FETCHING_MESSAGE_START });
+export const ADD_PARENT = 'ADD_PARENT';
+export const FETCHING_MESSAGE_START = 'FETCHING_MESSAGE_START';
+export const FETCHING_MESSAGE_SUCCESS = 'FETCHING_MESSAGE_SUCESS';
+export const FETCHING_MESSAGE_FAILURE = 'FETCHING_MESSAGE_FAILURE';
+
+export const getParent = () => dispatch => {
+    dispatch({ type: FETCHING_MESSAGE_START });
 
 
-//     axiosWithAuth
-//         .get('https://disney-parent-lambda.herokuapp.com/api')
-//         .then(res => {
-//             console.log(res);
-//             dispatch({ type: FETCHING_MESSAGE_SUCCESS, payload: res.data })
-//         })
-//         .catch(err => {
-//             console.log(err.toString())
-//             dispatch({ type: FETCHING_MESSAGE_FAILURE, payload: err.toString() })
-//         })
-// }
+    axiosWithAuth
+        .get('https://disney-parent-lambda.herokuapp.com/api')
+        .then(res => {
+            console.log(res);
+            dispatch({ type: FETCHING_MESSAGE_SUCCESS, payload: res.data })
+        })
+        .catch(err => {
+            console.log(err.toString())
+            dispatch({ type: FETCHING_MESSAGE_FAILURE, payload: err.toString() })
+        })
+}
 
 
 
 export const addParent = (newParent) => dispatch => {
-    axios
+    axiosWithAuth
     .post('https://disney-parent-lambda.herokuapp.com/api', newParent)
     .then(res => {
         console.log(res)
--- dp-front-end/src/index.js
+++ dp-front-end/src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM, {Provider} from 'react-dom';
 import './index.css';
 import App from './App';
+import store from './components/store'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    rootElement
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
