509a244837c91ffdd09c6514f570161d1cb43d7c
Eric Richards <esholundr@yahoo.com>
2019-10-22 15:33:12 -0600

    pathway change in Register

--- dp-front-end/src/components/Register.js
+++ dp-front-end/src/components/Register.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import axiosWithAuth from '../utils/axiosWithAuth';
+import axiosWithAuth from '../components/store/utils/axiosWithAuth';
 
 const Register = props => {
     const [form, setForm] = React.useState({ firstName: "", lastName: "", username: "", password: "" });
@@ -67,6 +67,5 @@ const Register = props => {
     );
 };
 
-export default Register; 
+export default Register;
 
- 
