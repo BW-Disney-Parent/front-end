27d05304b324606c0f171860a8865ec38610f879
Amber Sorensen <ambersorensen@ambers-mbp.lan>
2019-10-23 14:54:24 -0600

    fixed routing issue. log in now leads to parentform

--- dp-front-end/src/components/login.js
+++ dp-front-end/src/components/login.js
@@ -18,7 +18,7 @@ const Login = props => {
             .then(res => {
                 console.log(res);
                 localStorage.setItem("token", res.data.payload);
-                props.history.push("/");
+                props.history.push("/parentform");
             })
             .catch(err => {
                 console.log(err.response);
