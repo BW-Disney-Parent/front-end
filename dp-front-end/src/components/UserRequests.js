f7d6c4f725390aac04e0a84e44173618c0701fa5
Amber Sorensen <ambersorensen@ambers-mbp.lan>
2019-10-24 11:33:03 -0600

    finished writing bioler plate for UserRequest

--- dp-front-end/src/components/UserRequests.js
+++ dp-front-end/src/components/UserRequests.js
@@ -1,12 +1,13 @@
 import React, {useState, useEffect} from 'react';
 import axiosWithAuth from './store/utils/axiosWithAuth.js';
+import GetRequest from './GetRequest.js';
 
 const UserRequest = props => {
     const [userRequests, setUserRequests]= useState([]);
 
         useEffect(()=>{
             const awa= axiosWithAuth()
-            .get('/requests/all')
+            .get('/requests/all', {"filter": {"requesterUserID": 1}})
             .then(res=>{
                 console.log('UserRequest response', res)
                 setUserRequests(res.data)
@@ -17,5 +18,25 @@ const UserRequest = props => {
             console.log('data received in User request useEffect hook', props, random)
         }, [])
 
-        
+        if (props.isFetching) {
+            return <h2>Fetching...</h2>
+        }
+        console.log('props in UserRequest', props);
+
+        return (
+            <div>
+                {props.error && <p>{props.error}</p>}
+                {userRequests.map(request => (
+                    <Request key={request.id} request={request}/>
+                ))}
+            </div>
+        )
 }
+
+const mapStateToProps = state => {
+    return {
+        getRequest: state.request
+    }
+}
+
+export default connect(mapStateToProps,{getParent})(GetRequest);
